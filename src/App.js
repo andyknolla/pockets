3a6e9a4b8b35c2d8d11c4df2fa6aac78cc20278c
Andy Knolla <andyknolla@gmail.com>
2017-05-17 19:21:51 -0600

    implemented single material-ui card, added injectTapEventPlugin

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
-import './App.css';
+//import './App.css';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import Listing from './components/listing';
 
+injectTapEventPlugin();
+
 class App extends Component {
+
   render() {
     return (
-        <MuiThemeProvider>
+        <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
           <div className='App'>
             <div className="App-header">
               <h2>Listings</h2>
