550508f2c8c33a8621b87d4281881313f467d3d3
Andy Knolla <andyknolla@gmail.com>
2017-05-17 19:05:24 -0600

    1st static react child component inside MuiThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Listing from './components/listing';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Listings</h2>
-        </div>
-        
-      </div>
+        <MuiThemeProvider>
+          <div className='App'>
+            <div className="App-header">
+              <h2>Listings</h2>
+            </div>
+            <Listing />
+          </div>
+        </MuiThemeProvider>
     );
   }
 }
---
+++ src/components/listing.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Listing extends Component {
+  render() {
+    return (
+      <div className="listing">
+          <h2>Single Listing</h2>
+      </div>
+    );
+  }
+}
+
+export default Listing;
